b017686be1e5dd8d06c45ddce9610f96ddc35f0e
Dylan O <70405720+d-koding@users.noreply.github.com>
2024-02-27 09:47:54 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/about/<username>")
+def about_page(username):
+    return f"<h1>This is the about page of {username}</h1>"
